d1c23fca18225c893f9841b39860b97b1577c1fb
rushi-webdev <rushi.webdev@gmail.com>
2024-05-31 13:28:32 +0530

    Second Commit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send("Hello World Rushi")
 })
+app.get('/hello', (req, res) => {
+    res.send("Hello Rushi")
+})
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
